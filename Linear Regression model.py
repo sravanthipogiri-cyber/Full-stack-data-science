6bd8e84c54a559f2690b1db2147de3f467d6f945
sravanthipogiri-cyber <sravanthi.pogiri@gmail.com>
2025-12-17 09:58:04 +0530

    Add files via upload
    Linear Regression Model

--- Linear Regression model.py
+++ Linear Regression model.py
@@ -8,12 +8,21 @@ Created on Mon Dec 15 09:39:57 2025
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from scipy.stats import variation
+import scipy.stats as stats
+import pickle
+
+
+
+
 dataset=pd.read_csv(r"D:\Spyder-12 th\Salary_Data.csv")
 x=dataset.iloc[:,:-1]
 y=dataset.iloc[:,-1]
-from sklearn.model_selection import train_test_split
+
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
-from sklearn.linear_model import LinearRegression
+
 regressor= LinearRegression()
 regressor.fit(x_train,y_train)
 y_pred=regressor.predict(x_test)
@@ -35,3 +44,62 @@ bias_score=regressor.score(x_train, y_train)
 print (bias_score)
 variance_score=regressor.score(x_test,y_test)
 print(variance_score)
+
+#stats Integration to ML
+dataset.mean()
+dataset['Salary'].mean()
+dataset.mode()
+dataset.median()
+dataset['YearsExperience'].mean()
+dataset['Salary'].mean()
+
+dataset.var()
+dataset['YearsExperience'].var()
+dataset.std()
+dataset['Salary'].std()
+
+
+variation (dataset.values)
+variation(dataset['Salary'])
+#corelation
+dataset.corr()
+dataset['Salary'].corr(dataset['YearsExperience'])
+
+#Skewness
+dataset.skew()
+dataset['Salary'].skew()
+#Standard Error
+dataset.sem()
+#z-Score
+
+dataset.apply(stats.zscore)
+stats.zscore(dataset['Salary'])
+#Degree of freedom
+
+
+#Sum of square regression or #ssr
+y_mean=np.mean(y)
+SSR=np.sum((y_pred-y_mean)**2)
+print (SSR)
+
+y=y[0:6]
+SSE=np.sum((y-y_pred)**2)
+print (SSE)
+
+mean_total=np.mean(dataset.values)
+SST = np.sum(( dataset.values-mean_total)**2)
+print (SST)
+
+filename='linear_regression_model.pkl'
+with open (filename,'wb') as file:
+    pickle.dump(regressor,file)
+print("Model hasbeen Pickled and saved as linear_regression_model.pkl")
+
+
+
+
+
+
+
+
+
